5a040fb3f750c76d8ec32a8acc7b2c291b4a06c0
AbhijeetSR9996 <sabhijeet191@yahoo.com>
2023-01-29 00:14:15 +0530

    Add all my files

---
+++ CheckboxWithLabelNew.js
@@ -0,0 +1,26 @@
+import {useState} from 'react';
+
+export default function CheckboxWithLabel({
+  labelRef,
+  inputRef,
+  labelOn,
+  labelOff,
+}) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const onChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <label ref={labelRef}>
+      <input
+        ref={inputRef}
+        type="checkbox"
+        checked={isChecked}
+        onChange={onChange}
+      />
+      {isChecked ? labelOn : labelOff}
+    </label>
+  );
+}
